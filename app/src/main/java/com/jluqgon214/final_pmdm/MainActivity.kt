bcbc1e42f615655f69c4aef2551c75354c012f94
José Manuel Luque <jluqgon214@g.educaand.es>
2024-02-21 18:47:44 +0100

    Initial commit

---
+++ app/src/main/java/com/jluqgon214/final_pmdm/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.jluqgon214.final_pmdm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jluqgon214.final_pmdm.ui.theme.FinalPMDMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FinalPMDMTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    com.jluqgon214.final_pmdm.homepage.HomePage()
+                }
+            }
+        }
+    }
+}
